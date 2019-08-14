84cabfba185723721a3a84d1d66399e7015e7756
John Wright <johnlarrywright.ITspecialist@gmail.com>
2019-08-14 07:27:15 +0000

    added readme and append to a list

---
+++ README.md
@@ -0,0 +1,5 @@
+Twitterbot
+
+USAGE: Twitterbot.py
+Search for profile images on twitter
+Return list of profile images on twitter
--- twitterbot.py
+++ twitterbot.py
@@ -12,18 +12,21 @@ results = requests.get(url)
 #print(results.content)
 #soup = BeautifulSoup(results.content, 'html.parser')
 soup = BeautifulSoup(results.text, 'html.parser')
-# print(soup.div.prettify())
-#print(soup.divs.prettify())
+#print(soup.div.prettify())
+#print(soup.div.prettify())
 #print(soup)
 
-# for link in soup.find_all('a'):
-    # print(link.get('href'))
+for link in soup.find_all(''):
+    print(link.get('href'))
 
 profile_image = []    
 for link in soup.find_all('img'):
     source = link.get('src')
+    #filter out none type and pulling out http for action
     if source and source[:4]=='http':
-        print(source)
+        profile_image.append(source)
+        
+print(profile_image)
 
 
 
